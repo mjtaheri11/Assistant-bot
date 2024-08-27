f67ee0c9157267dec8abc9bf3fa81b68050f8903
mjtaheri11 <mj.taheri1996@gmail.com>
2024-08-27 09:12:46 +0000

    initiate database vdb parser modified

--- RagBot/ragbot/initiate_vdb.py
+++ RagBot/ragbot/initiate_vdb.py
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--persist_directory', default=config["database"]["persist_directory"],
                     type=str, help='The path of the persist directory')
     args = parser.parse_args()
-    main()
+    main(args)
