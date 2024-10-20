de705d696d2c61a16c83bf9a6026b0b451de4290
mjtaheri11 <mj.taheri1996@gmail.com>
2024-10-20 13:03:53 +0000

    new files added to the repo

--- Fa2SQL/run_app.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-streamlit run fa2sql/app.py --server.port 8502 --server.address 0.0.0.0
--- install_ollama.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -fsSL https://ollama.com/install.sh | sh
-ollama pull command-r:35b-v0.1-fp16
