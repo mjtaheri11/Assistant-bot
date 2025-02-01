0a3bf271652894c105075f1d1383d528e4a0047c
mjtaheri11 <mj.taheri1996@gmail.com>
2025-02-01 11:52:40 +0330

    bug fix: default value added to the session_id feedback

--- RagBot/api.py
+++ RagBot/api.py
@@ -89,7 +89,7 @@ class MakeResponse(BaseModel):
 class FeedbackRequest(BaseModel):
     message_id: str
     feedback_type: str
-    session_id: Optional[str]
+    session_id: Optional[str] = None  # Add default value
 
 
 class FeedbackResponse(BaseModel):
@@ -338,14 +338,10 @@ async def feedback(feedback_request: FeedbackRequest, request: Request):
     endpoint = "/feedback"
     REQUEST_COUNT.labels(endpoint=endpoint).inc()
     start_time = time.time()
-    
     try:
         # validate_feedback(feedback_request)
 
-        # session_id = get_session_id(request, feedback_request)
-        print(feedback_request)
-        print(request)
-        session_id = request.headers.get("Session-ID", None)
+        session_id = get_session_id(request, feedback_request)
         if session_id is None:
             session_id = feedback_request.session_id
 
