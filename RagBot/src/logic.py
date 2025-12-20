d98f46a9ae69dcabe0f827f4eb79cc36825d9f7c
Soroush AshouriSefat <sorousha1377@gmail.com>
2025-12-20 09:11:48 +0000

    Remove bussinuess_object_v2.py

--- RagBot/src/logic.py
+++ RagBot/src/logic.py
@@ -756,8 +756,6 @@ async def _determine_final_route(
             model_name=router_config["model_name"]
         )
         predictions, probabilities, max_prob = semantic_router_client.predict_sentences_input_embedding_and_sentences([utterance], [query_embedding])
-        import pdb 
-        pdb.set_trace()
         top_prediction = predictions[0][0]
         probabilities = list(probabilities)  # Convert to list to make it subscriptable
         if max_prob > alpha_threshold and ("همکاران" not in utterance) and (top_prediction != "illegal"):
