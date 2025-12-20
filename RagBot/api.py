8e6076e8e05dbf2f1f85c7df64b75a3966d998d4
Soroush AshouriSefat <sorousha1377@gmail.com>
2025-12-20 07:16:38 +0000

    Update API

--- RagBot/api.py
+++ RagBot/api.py
@@ -115,7 +115,7 @@ class ChatRequest(BaseModel):
     error_payload: Optional[str] = ""
     is_sync: Optional[bool] = True
     sql_mode: Optional[bool] = True  # Toggle between legacy and SQL agent mode
-    use_oss: Optional[bool] = False
+    use_oss: Optional[bool] = True
 
 class ChatResponse(BaseModel):
     message_id: str
